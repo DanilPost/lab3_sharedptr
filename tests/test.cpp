56e7f9eb43c8a76da5604514baee18d2810f84d6
Danil Postvaikin <postvaykin01@mail.ru>
2020-12-10 21:48:37 +0300

    l5

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,79 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Danil Postvaykin <postvaykin01@mail.ru>
 
 #include <gtest/gtest.h>
+#include <SharedPtr.hpp>
+
+class Testing
+{
+public:
+    int x;
+};
 
 TEST(Example, EmptyTest) {
     EXPECT_TRUE(true);
+}
+
+TEST(Constructor_test, point_test) {
+    SharedPtr <int> pointer1;
+    int* p = pointer1.get();
+    EXPECT_EQ(p, nullptr);
+}
+
+TEST(Method_test, test_get) {
+    int* point = new int(111);
+    SharedPtr <int> pointer1(point);
+    int* tmp = pointer1.get();
+    EXPECT_EQ(tmp, point);
+}
+
+TEST(Method_test, test_reset) {
+    int *point1 = new int (111);
+    int *point2 = new int (111);
+    SharedPtr <int> pointer1(point1);
+    SharedPtr <int> pointer2(point2);
+    pointer1.reset();
+    EXPECT_EQ(pointer1.get(), nullptr);
+}
+
+TEST(Method_test, test_reset_point) {
+    int *point = new int (111);
+    int *point1 = new int (111);
+    SharedPtr <int> pointer(point);
+    pointer.reset(point1);
+    EXPECT_EQ(pointer.get(), point1);
+}
+
+TEST(Method_test, test_swap) {
+    int *point1 = new int (111);
+    int *point2 = new int (111);
+    SharedPtr <int> pointer1(point1);
+    SharedPtr <int> pointer2(point2);
+    pointer1.swap(pointer2);
+    EXPECT_EQ(pointer1.get(), point2);
+    EXPECT_EQ(pointer2.get(), point1);
+}
+
+TEST(Operator_test, test_bool) {
+    SharedPtr <int> pointer;
+    bool ptr = pointer;
+    int* point = new int(111);
+    SharedPtr <int> pointer2(point);
+    bool ptr2 = pointer2;
+    EXPECT_EQ(ptr, false);
+    EXPECT_EQ(ptr2, true);
+}
+
+TEST(Operator_test, test_arrow) {
+    auto test_arrow = new Testing;
+    test_arrow -> x = 111;
+    SharedPtr <Testing> pointer(test_arrow);
+    EXPECT_EQ(pointer->x, test_arrow->x);
+}
+
+TEST(Operator_test, test_equality) {
+    int *point = new int (111);
+    SharedPtr <int> pointer(point);
+    SharedPtr <int> pointer2;
+    pointer2 = pointer;
+    EXPECT_EQ(pointer2.get(), pointer.get());
 }
